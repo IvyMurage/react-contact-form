8a40799b300d588502d9a39a9ac32abf8b930ee9
Murage-Ivy <ivymurage2000@gmail.com>
2024-07-30 19:03:07 +0200

    chore: add styles to the form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,18 +5,20 @@ import Form from './ContactForm'
 function Contact() {
     const [info] = useState(content)
     const contactInfo = Object.entries(info.contact_info)
-    const contactList = contactInfo.map(info => <div>
-        <h3>{info[0]}</h3>
+    const contactList = contactInfo.map(info => <div className='pb-3'>
+        <h3 className='pb-2'>{info[0]}</h3>
         <p>{info[1]}</p>
     </div>)
     return (
-        <div className='text-white grid border-2 p-12 items-center m-auto grid-cols-2' >
+        <div className='text-white flex justify-between p-12 items-center m-auto grid-cols-2' >
             <div className=''>
-                <h1 className='font-bold  text-4xl'>{info.title.toUpperCase()}</h1>
-                <p className=''>{info.content}</p>
+                <h1 className=' font-bold  text-4xl'>{info.title.toUpperCase()}</h1>
+                <div className='w-14 mt-3 h-1 bg-blue-100'></div>
+                <p className='mt-5 text-gray-200 '>{info.content}</p>
                 <Form />
             </div>
-            <div className='place-self-end border-2 h-full'>
+            <div className=' space-y-7 border-2 h-full'>
+                <h2 className='text-xl font-medium'>Contact Info</h2>
                 {contactList}
             </div>
         </div>
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -38,28 +38,31 @@ function Form() {
   console.log(contacts)
 
   return (
-    <form onSubmit={handleSubmit} >
-      <div className='flex flex-col'>
-        <Input
-          value={contact.name}
-          placeholder='Name'
-          type='text'
-          onChange={handleChange}
-          name='name'
-        />
-        <Input
-          value={contact.email}
-          placeholder='Email'
-          type='email'
-          onChange={handleChange}
-          name='email'
-        />
-        <Input
+    <form onSubmit={handleSubmit} className='py-5 text-black text-sm mt-5' >
+      <div className='flex flex-col justify-between'>
+        <div className='grid grid-cols-2 gap-5'>
+          <Input
+            value={contact.name}
+            placeholder='Name'
+            type='text'
+            onChange={handleChange}
+            name='name'
+          />
+          <Input
+            value={contact.email}
+            placeholder='Email'
+            type='email'
+            onChange={handleChange}
+            name='email'
+          />
+        </div>
+
+        <textarea
+          rows={4}
           value={contact.message}
           placeholder='Message'
-          type='text'
-          onChange={handleChange}
           name='message'
+          className='h-auto mb-4'
         />
       </div>
       <Button type='submit' placeholder='SEND MESSAGE' />
